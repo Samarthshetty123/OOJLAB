28af3d5fcd01a9527793bbe2cc236c94be14ddb6
Samarthshetty123 <63661104+Samarthshetty123@users.noreply.github.com>
2020-11-20 14:48:06 +0530

    Add files via upload

---
+++ pac/Demo.java
@@ -0,0 +1,9 @@
+import p1.*;
+class Demo
+{
+	public static void main(String args[])
+	{
+		Main1 M1=new Main1();
+                M1.display();
+	}
+}
---
+++ pac/Main1.java
@@ -0,0 +1,8 @@
+package p1;
+public class Main1{
+ public int a=4;
+public void display()
+{
+   System.out.println("result="+a);
+}
+}
